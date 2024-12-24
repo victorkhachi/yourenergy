146fb2983822191dc8755c7a1678bf710ec1af0b
Onyedikachi Obi <victorkhachi@gmail.com>
2024-12-24 01:12:53 +0100

    update

--- src/components/portfolio/ProjectGrid.tsx
+++ src/components/portfolio/ProjectGrid.tsx
@@ -7,6 +7,7 @@ import Building from "../../assets/neatWOrk.jpeg"
 import SL from "../../assets/slAbo.jpeg"
 import Roof from "../../assets/rooofTop.jpeg"
 import Roof2 from "../../assets/roof.jpeg"
+import Grid from "../../assets/grid.jpg"
 
 
 const projects: Project[] = [
@@ -14,7 +15,7 @@ const projects: Project[] = [
     id: '1',
     title: 'Modern Home Solar Installation',
     type: 'residential',
-    location: 'New Jersey',
+    location: 'Oddington, Maryland',
     capacity: 10.5,
     energySavings: 15000,
     description: 'Complete solar panel installation for a modern family home.',
@@ -30,7 +31,7 @@ const projects: Project[] = [
     title: 'Commercial Street Light ',
     type: 'commercial',
     location: 'Asaba, Nigeria',
-    capacity: 50.2,
+    capacity: 100.2,
     energySavings: 75000,
     description: 'Large-scale commercial installation for a Sub-Urban area in Nigerai.',
     imageUrl:SL,
@@ -39,7 +40,7 @@ const projects: Project[] = [
     id: '3',
     title: 'Eco-Friendly Villa',
     type: 'residential',
-    location: 'Virginia , CA',
+    location: 'Virginia ',
     capacity: 15.8,
     energySavings: 20000,
     description: 'Luxury home with integrated solar and battery storage.',
@@ -49,11 +50,31 @@ const projects: Project[] = [
     id: '4',
     title: 'Solar Grid Installation',
     type: 'commercial',
-    location: 'Mary, CA',
+    location: 'Lekki ,Lagos ,Nigeria ',
     capacity: 200.5,
     energySavings: 300000,
-    description: 'Massive solar installation for a home.',
+    description: ' solar installation for a new house in  Lagos, Nigeria.',
     imageUrl: Roof2,
+  }, 
+  {
+    id: '4',
+    title: 'Solar Grid Installation',
+    type: 'commercial',
+    location: 'MaryLand, ',
+    capacity: 200.5,
+    energySavings: 300000,
+    description: 'Massive solar installation for an Industrial Estate.',
+    imageUrl: 'https://images.unsplash.com/photo-1611365892117-00ac5ef43c90?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  }, 
+  {
+    id: '4',
+    title: 'Solar Grid Installation',
+    type: 'commercial',
+    location: 'Ogun State, Nigeria',
+    capacity: 200.5,
+    energySavings: 300000,
+    description: 'Massive solar installation for a Farm estate.',
+    imageUrl: Grid,
   },
 ];
 
